1d49ced56aafb1d0a2def4ff061ea15603037aa3
Nam Ki Hun <bye0651@naver.com>
2023-07-04 22:53:09 +0900

    [level 0] Title: 두 수의 합, Time: 0.02 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120802. 두 수의 합/두 수의 합.js
@@ -0,0 +1,16 @@
+/*
+function solution(num1, num2) {
+    return num1 + num2
+}
+*/
+
+/*
+const solution = (num1, num2) => num1 + num2
+*/
+
+function solution(num1, num2) {
+    const answer = num1 + num2
+    return answer
+}
+
+
