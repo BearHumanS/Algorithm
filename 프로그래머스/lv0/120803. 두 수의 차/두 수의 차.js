74f33ba4138d7281050b923246fd624e44381f90
Nam Ki Hun <bye0651@naver.com>
2023-07-04 22:53:19 +0900

    [level 0] Title: 두 수의 차, Time: 0.02 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120803. 두 수의 차/두 수의 차.js
@@ -0,0 +1,13 @@
+/*
+const solution = (num1, num2) => num1 - num2
+
+
+function solution(num1, num2) {
+    return num1 - num2
+}
+*/
+
+function solution(num1, num2) {
+    const answer = num1 - num2;
+    return answer;
+}
