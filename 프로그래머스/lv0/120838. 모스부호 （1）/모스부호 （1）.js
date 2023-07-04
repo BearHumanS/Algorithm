76b058dcb2f56ab059850761f7114c7f19d103f2
Nam Ki Hun <bye0651@naver.com>
2023-07-04 23:02:31 +0900

    [level 0] Title: 모스부호 (1), Time: 0.07 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120838. 모스부호 （1）/모스부호 （1）.js
@@ -0,0 +1,29 @@
+function solution(letter) {
+    const morse = [".-","-...","-.-.","-..",".","..-.","--.","....","..",".---","-.-",".-..","--","-.","---",".--.","--.-",".-.","...","-","..-","...-",".--","-..-","-.--","--.."]
+    const toArr = letter.split(' ')
+    
+    const toAscii = toArr.map(code => morse.indexOf(code) + 97)
+    
+    return String.fromCharCode(...toAscii)
+}
+
+/*
+function solution(letter) {
+    const morse = { 
+    '.-':'a','-...':'b','-.-.':'c','-..':'d','.':'e','..-.':'f',
+    '--.':'g','....':'h','..':'i','.---':'j','-.-':'k','.-..':'l',
+    '--':'m','-.':'n','---':'o','.--.':'p','--.-':'q','.-.':'r',
+    '...':'s','-':'t','..-':'u','...-':'v','.--':'w','-..-':'x',
+    '-.--':'y','--..':'z'
+}
+    const codes = letter.split(' ')
+    let result = ''
+    for (let i = 0; i < codes.length; i++) {
+        const code = codes[i]
+        if (code in morse) {
+            result += morse[code]
+        }
+    }
+    return result
+}
+*/
