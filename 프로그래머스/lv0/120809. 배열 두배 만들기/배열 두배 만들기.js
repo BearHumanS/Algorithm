e3fae97929924813d2b68a4a22199227d6319787
Nam Ki Hun <bye0651@naver.com>
2023-07-04 22:54:48 +0900

    [level 0] Title: 배열 두배 만들기, Time: 0.07 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120809. 배열 두배 만들기/배열 두배 만들기.js
@@ -0,0 +1,14 @@
+/*
+function solution(numbers) {
+    const double = numbers.map(num => num*2)
+    return double
+}
+*/
+
+/*
+function solution(numbers) {
+	return numbers.map(number => number * 2)
+}
+*/
+
+const solution = numbers => numbers.map(number => number * 2)
