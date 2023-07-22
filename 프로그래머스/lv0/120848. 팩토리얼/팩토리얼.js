2ae4708fe4745144488514226a55c8c83b0c2d9e
BearHuman <bye0651@naver.com>
2023-07-22 22:10:01 +0900

    [level 0] Title: 팩토리얼, Time: 0.06 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120848. 팩토리얼/팩토리얼.js
+++ 프로그래머스/lv0/120848. 팩토리얼/팩토리얼.js
@@ -2,8 +2,21 @@ function solution(n) {
     let I = 1;
     let Array = []
     for (let i = 1; I <= n; i++) {
+        I *= i;
+        if (I > n) {
+            break;
+        }
+        Array.push(I);
+    }
+    return Array.length;
+}
+
+
+/*function solution(n) {
+    let I = 1;
+    let Array = []
+    for (let i = 1; I <= n; i++) {
        Array.push(I *= i)
     }
     const result = Array.splice(-1)
-    return Array.length
-}
+    return Array.length*/
