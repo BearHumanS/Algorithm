995c95586ca09e2814cdcc5b7603aaeb497419ba
BearHuman <bye0651@naver.com>
2023-08-15 22:15:23 +0900

    [level 0] Title: 숨어있는 숫자의 덧셈 (1), Time: 0.06 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120851. 숨어있는 숫자의 덧셈 （1）/숨어있는 숫자의 덧셈 （1）.js
+++ 프로그래머스/lv0/120851. 숨어있는 숫자의 덧셈 （1）/숨어있는 숫자의 덧셈 （1）.js
@@ -1,6 +1,12 @@
 function solution(my_string) {
+    return [...my_string].reduce((a, b) => Number(b) ? +a + +b : a,0)
+}
+
+/*
+function solution(my_string) {
     const regex = /[^0-9]/g;
     const number = my_string.replace(regex, "").split('')
     const result = number.map(Number)
     return result.reduce((a, b) => a + b)
 }
+*/
