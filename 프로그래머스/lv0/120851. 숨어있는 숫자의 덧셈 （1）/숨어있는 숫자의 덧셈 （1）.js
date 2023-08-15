9bb91443d771733741896849597247f04c522cc4
BearHuman <bye0651@naver.com>
2023-08-15 22:21:52 +0900

    [level 0] Title: 숨어있는 숫자의 덧셈 (1), Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120851. 숨어있는 숫자의 덧셈 （1）/숨어있는 숫자의 덧셈 （1）.js
+++ 프로그래머스/lv0/120851. 숨어있는 숫자의 덧셈 （1）/숨어있는 숫자의 덧셈 （1）.js
@@ -1,3 +1,8 @@
+// +a, +b를 한 이유
+// +는 단항 더하기 연산자이다.
+// 피연산자 앞에 위치하며 피연산자를 평가하지만, 
+// 만약 피연산자가 숫자가 아니라면 숫자로 변환을 시도한다.
+// 단항 더하기 연산자 대신에 Number함수를 사용해도 된다.
 function solution(my_string) {
     return [...my_string].reduce((a, b) => Number(b) ? +a + +b : a,0)
 }
