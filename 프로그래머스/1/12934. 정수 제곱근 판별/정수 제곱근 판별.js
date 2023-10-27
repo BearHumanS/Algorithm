3d0805cbaa4fc4fa425f575e1e1611143746a115
BearHuman <bye0651@naver.com>
2023-10-27 20:09:44 +0900

    [level 1] Title: 정수 제곱근 판별, Time: 0.03 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/1/12934. 정수 제곱근 판별/정수 제곱근 판별.js
+++ 프로그래머스/1/12934. 정수 제곱근 판별/정수 제곱근 판별.js
@@ -1,10 +1,18 @@
 function solution(n) {
+    const x = Math.sqrt(n)
+    if (x % 1 === 0) {
+        return (x + 1)**2
+    }
+    return -1
+}
+
+/*
+function solution(n) {
     let nNumber;
     for (let i = 1; i <= n; i++) {
         if (n % i === 0) {
             const number = i
         } 
-        
         if (i * i === n) {
           nNumber = i
             return (nNumber + 1)**2
@@ -12,3 +20,4 @@ function solution(n) {
     }
     return -1
 }
+*/
