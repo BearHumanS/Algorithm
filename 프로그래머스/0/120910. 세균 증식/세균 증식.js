b62773df959b674021ec274ae5745477162bb9dd
BearHuman <bye0651@naver.com>
2024-05-07 21:01:40 +0900

    [level 0] Title: 세균 증식, Time: 0.03 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/120910. 세균 증식/세균 증식.js
+++ 프로그래머스/0/120910. 세균 증식/세균 증식.js
@@ -1,7 +1,18 @@
+/*
 function solution(n, t) {
     for (let i = 0; i < t; i ++) {
         n *=2
     }
     
     return n
+}
+*/
+
+const solution = (n, t) => {
+    
+    if ( t === 0) {
+        return n
+    }
+    
+    return solution(n * 2, t - 1)
 }
