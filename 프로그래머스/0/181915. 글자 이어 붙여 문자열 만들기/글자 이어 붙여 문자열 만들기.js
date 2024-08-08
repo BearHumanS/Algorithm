43f358bab5199599f599d84c5f26cfd901c81957
BearHuman <bye0651@naver.com>
2024-08-08 23:55:41 +0900

    [level 0] Title: 글자 이어 붙여 문자열 만들기, Time: 5.91 ms, Memory: 34.8 MB -BaekjoonHub

--- 프로그래머스/0/181915. 글자 이어 붙여 문자열 만들기/글자 이어 붙여 문자열 만들기.js
+++ 프로그래머스/0/181915. 글자 이어 붙여 문자열 만들기/글자 이어 붙여 문자열 만들기.js
@@ -1,4 +1,16 @@
 function solution(my_string, index_list) {
+    let answer = []
+    
+    for (let i = 0; i < index_list.length; i++) {
+        answer.push([...my_string][index_list[i]])
+    }
+    
+    return answer.join('')
+}
+
+/*
+메모리: 34.7 MB, 시간: 5.67 ms
+function solution(my_string, index_list) {
     let answer = ''
     
     for (let i = 0; i < index_list.length; i++) {
@@ -7,3 +19,4 @@ function solution(my_string, index_list) {
     
     return answer
 }
+*/
