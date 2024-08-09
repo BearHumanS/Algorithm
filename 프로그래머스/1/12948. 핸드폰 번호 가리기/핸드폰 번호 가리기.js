1fd420e7a4236fb211a98f04e916c4d00c4f0ed2
BearHuman <bye0651@naver.com>
2024-08-10 02:58:51 +0900

    [level 1] Title: 핸드폰 번호 가리기, Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/1/12948. 핸드폰 번호 가리기/핸드폰 번호 가리기.js
+++ 프로그래머스/1/12948. 핸드폰 번호 가리기/핸드폰 번호 가리기.js
@@ -1,10 +1,13 @@
 function solution(phone_number) {
-    
-    const arr = [...phone_number]
+    /*
+     const arr = [...phone_number]
     const lastNumber = arr.slice(-4)
     
     const filtered = arr.filter((_, i) => i < arr.length - 4)
     const masking = filtered.map(item => item = "*")
     
     return masking.concat(lastNumber).join('')
+    */
+    
+    return [...phone_number].map((item, i) => i < [...phone_number].length - 4 ? "*" : item).join('')
 }
