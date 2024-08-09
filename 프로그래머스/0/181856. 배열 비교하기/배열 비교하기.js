b36d82d53c8060fd427e407c50f99568e15de911
BearHuman <bye0651@naver.com>
2024-08-09 19:35:14 +0900

    [level 0] Title: 배열 비교하기, Time: 0.11 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/181856. 배열 비교하기/배열 비교하기.js
+++ 프로그래머스/0/181856. 배열 비교하기/배열 비교하기.js
@@ -2,6 +2,7 @@ function solution(arr1, arr2) {
     const arr1Sum = arr1.reduce((a, b) => a + b, 0)
     const arr2Sum = arr2.reduce((a, b) => a + b, 0)
     
+  /*
     if (arr1.length > arr2.length) {
         return 1
     } else if (arr1.length < arr2.length) {
@@ -14,5 +15,8 @@ function solution(arr1, arr2) {
         return 0
     }
     
+  */
+    
+    return arr1.length > arr2.length ? 1 : arr1.length < arr2.length ? -1 : arr1Sum > arr2Sum ? 1 : arr1Sum < arr2Sum? -1 : arr1Sum === arr2Sum && 0
     
 }
