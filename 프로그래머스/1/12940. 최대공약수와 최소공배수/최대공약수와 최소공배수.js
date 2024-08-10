c50bff277447e8f0da6d89864f547b8bc217f2d3
BearHuman <bye0651@naver.com>
2024-08-10 21:38:55 +0900

    [level 1] Title: 최대공약수와 최소공배수, Time: 0.25 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/1/12940. 최대공약수와 최소공배수/최대공약수와 최소공배수.js
+++ 프로그래머스/1/12940. 최대공약수와 최소공배수/최대공약수와 최소공배수.js
@@ -12,3 +12,26 @@ function solution(n, m) {
     
     return [arr[arr.length - 1], (min * max) / arr[arr.length - 1]]
 }
+
+/*
+function gcd(a, b){
+    while(b !== 0){
+        let temp = b;
+        b = a % b;
+        a = temp;
+    }
+    return a;
+} << 유클리드 호제법으로 최대 공약수
+
+function lcm(a, b){
+    return (a * b) / gcd(a,b);
+} << 최대 공배수
+
+function solution(n, m) {
+    let answer = [];
+    answer = [gcd(n,m), lcm(n,m)];
+
+    return answer;
+}
+
+*/
