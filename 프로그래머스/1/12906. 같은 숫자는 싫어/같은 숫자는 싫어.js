ee1a6a81c23960d2702df46baa2ef5c9ad1bc4d1
BearHuman <bye0651@naver.com>
2024-08-10 22:13:20 +0900

    [level 1] Title: 같은 숫자는 싫어, Time: 32.08 ms, Memory: 82.9 MB -BaekjoonHub

--- 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.js
+++ 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.js
@@ -1,16 +1,9 @@
-function solution(arr) {
-   let result = []
-   
-   for (let i = 0; i < arr.length; i++) {
-       if (arr[i] !== arr[i + 1]) {
-           result.push(arr[i])
-       }
-   }
-    
-    return result
-}
+function solution(arr)
+{
+    var answer = [];
 
-/*
-배열 arr의 각 원소는 숫자 0 ~ 9
-arr에서 연속적으로 나타나는 숫자는 하나만 남기고 전부 제거
-*/
+    // [실행] 버튼을 누르면 출력 값을 볼 수 있습니다.
+    console.log('Hello Javascript')
+    
+    return answer;
+}
