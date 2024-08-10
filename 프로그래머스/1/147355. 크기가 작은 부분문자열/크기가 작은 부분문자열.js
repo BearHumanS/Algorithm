6c802c7fe026b794e838ed9035005c62d9f79776
BearHuman <bye0651@naver.com>
2024-08-10 22:52:01 +0900

    [level 1] Title: 크기가 작은 부분문자열, Time: 43.17 ms, Memory: 37.9 MB -BaekjoonHub

---
+++ 프로그래머스/1/147355. 크기가 작은 부분문자열/크기가 작은 부분문자열.js
@@ -0,0 +1,16 @@
+function solution(t, p) {
+    const arr = [...t]
+    let answer = []
+    
+    for (let i = 0; i <= arr.length - [...p].length; i++) {
+        let temp = ''
+        for (let j = 0; j < [...p].length; j++) {
+            temp += t[i + j];
+        }
+        answer.push(temp)
+    }
+    
+    
+    return answer.map(e => e <= p).filter(x => x === true).length
+}
+
