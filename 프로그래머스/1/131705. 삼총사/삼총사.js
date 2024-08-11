10c60bd3f5348d87f60aa169ec44c660c605c292
BearHuman <bye0651@naver.com>
2024-08-11 23:08:38 +0900

    [level 1] Title: 삼총사, Time: 0.21 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/1/131705. 삼총사/삼총사.js
+++ 프로그래머스/1/131705. 삼총사/삼총사.js
@@ -14,6 +14,24 @@ function solution(number) {
 }
 
 /*
+function solution(number) {
+    let count = 0;
+    
+    for (let i = 0; i < number.length - 2; i++) {
+        for (let j = i + 1; j < number.length - 1; j++) {
+            for (let k = j + 1; k < number.length; k++) {
+                if (number[i] + number[j] + number[k] === 0) {
+                    count++;
+                }
+            }
+        }
+    }
+    
+    return count;
+}
+*/
+
+/*
 학생들은 고유의 정수 번호를 지님
 
 학생 3명의 정수 번호의 합이 0일 때 삼총사라고 불림
