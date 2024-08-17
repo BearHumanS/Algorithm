be73f5b979eadf700caefd79c2109f49b826a87b
BearHuman <bye0651@naver.com>
2024-08-17 21:22:01 +0900

    Time: 63 ms (90.49%), Space: 57.3 MB (81.91%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.js
+++ 0053-maximum-subarray/0053-maximum-subarray.js
@@ -2,7 +2,7 @@
  * @param {number[]} nums
  * @return {number}
  */
-var maxSubArray = function(nums) {
+/* var maxSubArray = function(nums) {
     let local = 0
     let global = -Infinity
 
@@ -12,6 +12,18 @@ var maxSubArray = function(nums) {
     })
 
     return global
-};
+}; */
 // 정수 배열 nums
 // 배열의 요소 연속되는 요소의 합이 제일 큰 경우의 요소들을 찾아서 합계를 반환
+
+var maxSubArray = function(nums) {
+    let curr_sum = nums[0]
+    let max_sum = nums[0]
+
+    for (let i = 1; i < nums.length; i++) {
+        curr_sum = Math.max(nums[i], curr_sum + nums[i])
+        max_sum = Math.max(max_sum, curr_sum)
+    }
+
+    return max_sum
+};
